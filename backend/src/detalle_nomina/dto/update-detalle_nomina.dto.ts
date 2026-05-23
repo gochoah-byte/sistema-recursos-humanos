f5e7b1af4412052a4da69ef356a3cde8677b7c29
gochoah-byte <gochoah@miumg.edu.gt>
2026-05-22 22:19:55 -0600

    Eliminación de duplicado

--- backend/src/detalle_nomina/dto/update-detalle_nomina.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDetalleNominaDto } from './create-detalle_nomina.dto';
-
-export class UpdateDetalleNominaDto extends PartialType(CreateDetalleNominaDto) {}
--- backend/src/periodo_nomina/dto/update-periodo_nomina.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePeriodoNominaDto } from './create-periodo_nomina.dto';
-
-export class UpdatePeriodoNominaDto extends PartialType(CreatePeriodoNominaDto) {}
